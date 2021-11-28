e8bf08df9225466e0513e5cef308c64659cedcc5
Oleksii <oleksiy@noroutine.me>
2021-11-28 16:34:41 +0100

    GDP build pod tolerations

--- binderhub/build.py
+++ binderhub/build.py
@@ -454,6 +454,13 @@ class KubernetesBuildExecutor(BuildExecutor):
                         value="user",
                         effect="NoSchedule",
                     ),
+                    # GDP
+                    client.V1Toleration(
+                        key='gdp-jupyterhub/dedicated',
+                        operator='Equal',
+                        value='all',
+                        effect='NoSchedule',
+                    ),
                 ],
                 node_selector=self.node_selector,
                 volumes=volumes,
